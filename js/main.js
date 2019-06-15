0426b54409e3d670dcee3bba78dd2caa2fc1034d
Sone Gillis <sone_gillis@yahoo.com>
2019-06-16 00:21:19 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    let get_work_orders_url = "​https://www.hatchways.io/api/assessment/work_orders";
+    let get_work_orders_url = "​https://www.hatchways.io/api/assessment/work_orders/";
     getWorkOrders(get_work_orders_url);
 });
 
@@ -7,7 +7,7 @@ function getWorkOrders(get_work_orders_url){
     $.ajax({
         type: "GET",
         url: get_work_orders_url,
-        dataType: "json",
+        dataType: "jsonp",
         success: function(result, status, xhr){
             alert(result);
         },
@@ -19,3 +19,4 @@ function getWorkOrders(get_work_orders_url){
     });
 
 }
+
