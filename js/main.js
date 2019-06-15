020a584aa15e66e1adbe210f9b4c3c59dc3bf525
Sone Gillis <sone_gillis@yahoo.com>
2019-06-16 00:11:45 +0100

    initial status

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    let get_work_orders_url = "​http://www.hatchways.io/api/assessment/work_orders";
+    getWorkOrders(get_work_orders_url);
+});
+
+function getWorkOrders(get_work_orders_url){
+    $.ajax({
+        type: "GET",
+        url: get_work_orders_url,
+        dataType: "jsonp",
+        success: function(result, status, xhr){
+            alert(result);
+        },
+        error: function(xhr, status, error){
+            alert(status);
+            alert(error);
+            alert(xhr.responseText);
+        }
+    });
+
+}
